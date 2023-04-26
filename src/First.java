244ef5671b9ecad07a5b4330c5b8209f055bad43
Administrator <tncjs3598@naver.com>
2023-04-26 22:29:34 +0900

    Study

--- src/First.java
+++ src/First.java
@@ -7,7 +7,7 @@ public class First {
         String name = "성수천";
         System.out.println("나이를 입력하세요");
 
-        String a = sc.nextLine();
+        String a = sc.nextLine(); 
         int age = Integer.parseInt(a);
 
         System.out.println("제나이는 " + age + "살 이고 이름은 " + name +"입니다.");
